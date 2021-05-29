9ea98137bbb512700311f9570f4835dbc0fab552
Dan Negvesky <dnegvesky@reflexces.com>
2021-05-28 22:02:19 -0400

    change ifconfig location for poky

--- recipes-achilles/achilles-usb-gadget/files/achilles-gadget-init.sh
+++ recipes-achilles/achilles-usb-gadget/files/achilles-gadget-init.sh
@@ -5,6 +5,6 @@ modprobe g_multi file=/usr/share/achilles-usb-gadget/achilles_fat_image.img
 sleep 5
 
 #rm /var/lib/misc/udhcpd.leases
-/sbin/ifconfig usb0 hw ether 00:01:02:03:0A:10
-/sbin/ifconfig usb0 192.168.10.1 netmask 255.255.255.0
+/bin/ifconfig usb0 hw ether 00:01:02:03:0A:10
+/bin/ifconfig usb0 192.168.10.1 netmask 255.255.255.0
 /usr/sbin/udhcpd -fS -I 192.168.10.1 /etc/udhcpd.conf
